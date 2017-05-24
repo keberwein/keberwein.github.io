e4387f47b6cb377f71a9531b7d16b68e98131f0f
Keberwein <kristian.eberwein@yahoo.com>
2017-05-24 11:51:59 -0400

    added update packages post

--- _template/knit_post.R
+++ _template/knit_post.R
@@ -3,4 +3,4 @@ library(knitr)
 
 setwd("~/Documents/keberwein.github.io/_Rmd")
 
-krisUtils::KnitPost(fileName="2017-05-22-shiny.nginx.Rmd", category="r", fig=F)
+krisUtils::KnitPost(fileName="2017-05-24-upgrade-R-packages.Rmd", category="r", fig=F)
