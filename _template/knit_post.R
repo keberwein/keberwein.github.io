04b4a1f6afe36b80639d039692b1d2dbb703871a
Keberwein <kristian.eberwein@yahoo.com>
2017-08-22 07:58:32 -0400

    added tidybls

--- _template/knit_post.R
+++ _template/knit_post.R
@@ -3,4 +3,4 @@ library(knitr)
 
 setwd("~/Documents/keberwein.github.io/_Rmd")
 
-krisUtils::KnitPost(fileName="2017-06-20-lgwoba.Rmd", category="r", fig=T)
+krisUtils::KnitPost(fileName="2017-08-22-tidyblscar.Rmd", category="r", fig=T)
