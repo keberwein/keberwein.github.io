2f8afa326a21892a59bb1d114f29f7dcfba9145d
keberwein <kristian.eberwein@yahoo.com>
2018-01-25 15:59:28 -0500

    test

--- _template/knit_post.R
+++ _template/knit_post.R
@@ -3,4 +3,4 @@ library(knitr)
 
 setwd("~/Documents/keberwein.github.io/_Rmd")
 
-krisUtils::KnitPost(fileName="2017-08-22-tidyblscar.Rmd", category="r", fig=T)
+krisUtils::KnitPost(fileName="2017-08-23-tidyblscar.Rmd", category="r", fig=T)
